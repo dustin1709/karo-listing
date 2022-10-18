94389307bc8f738d776aebeb44d95926c634fc07
dustin1709 <duong.chu.vu@lalumiere.org>
2022-10-18 17:44:26 +0700

    edit responsive navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -29,7 +29,7 @@ const Header = () => {
       </div>
       
       <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden'>
-      {open ? <img src={Bar}/>:<img src={Close}/>}
+      {!open ? <img src={Bar}/>:<img src={Close}/>}
       </div>
 
       <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-18 ':'top-[-490px]'}`}>
